6dfc8caddca7f287fe7ec106743ea0e7f9139b8d
KartikeyaCSE <2311cs010166@mallareddyuniversity.ac.in>
2025-01-06 11:46:16 +0530

    first commit

---
+++ JAVA CRACK/src/day4_conditional.java
@@ -0,0 +1,5 @@
+public class day4_conditional {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ JAVA CRACK/src/session_3.java
@@ -0,0 +1,14 @@
+public class session_3 {
+    public static void main(String[] args) {
+        String str = "Hello";
+        String str1 = "mountain";
+        int len = str.length(); 
+        System.out.println(str.length());
+        System.out.println(str.charAt(len - 1));
+        System.out.println(str.substring(len - 1));
+
+
+
+    }
+    
+}
